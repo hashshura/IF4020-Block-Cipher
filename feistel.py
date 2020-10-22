c41d3e9ee684dbac1b3b7df550ab48a0a05a018e
Juniardi Akbar <juniardiakbar@gmail.com>
2020-10-22 20:54:39 +0700

    Add S-boxex

--- feistel.py
+++ feistel.py
@@ -8,6 +8,17 @@ BLOCKSIZE_BITS = 64
 PATH_TO_FILES = "Files/"
 SECRET = "3f788083-77d3-4502-9d71-21319f1792b6"
 
+sb1 = [ 4, 10, 9, 2, 13, 8, 0, 14, 6, 11, 1, 12, 7, 15, 5, 3 ]
+sb2 = [ 14, 11, 4, 12, 6, 13, 15, 10, 2, 3, 8, 1, 0, 7, 5, 9 ]
+sb3 = [ 5, 8, 1, 13, 10, 3, 4, 2, 14, 15, 12, 7, 6, 0, 9, 11 ]
+sb4 = [ 7, 13, 10, 1, 0, 8, 9, 15, 14, 4, 6, 12, 11, 2, 5, 3 ]
+sb5 = [ 6, 12, 7, 1, 5, 15, 13, 8, 4, 10, 9, 14, 0, 3, 11, 2 ]
+sb6 = [ 4, 11, 10, 0, 7, 2, 1, 13, 3, 6, 8, 5, 9, 12, 15, 14 ]
+sb7 = [ 13, 11, 4, 1, 3, 15, 5, 9, 0, 10, 14, 7, 6, 8, 2, 12 ]
+sb8 = [ 1, 15, 13, 0, 5, 7, 10, 4, 9, 2, 3, 14, 6, 11, 8, 12 ]
+
+sb = [ sb1, sb2, sb3, sb4, sb5, sb6, sb7, sb8 ]
+
 def main(argv):
     decrypt = False
     encrypt = False
@@ -188,8 +199,18 @@ def scramble(x, i, k):
     k = stobin(k)
     x = stobin(str(x))
 
+    if (len(x) == 32) :
+        out = ""
+        for i in range(8):
+            val = bintoint(x[i*4:(i*4) + 4])
+            out += bin(sb[i].index(val))[2:].zfill(4)
+
+        out = out[4:len(out)] + out[0:4]
+    else:
+        out = x
+
     k = bintoint(k)
-    x = bintoint(x)
+    x = bintoint(out)
 
     res = pow((x * k), i)
     res = itobin(res)
